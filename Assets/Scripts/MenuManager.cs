8de6637918201477428f0ed33b9ecf769d4e21f3
Mateusz Wójtowicz <matiwojt@gmail.com>
2026-04-20 11:58:39 +0200

    improve script readability, SerializeField for direct Unity Scene strings modifications

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,37 +4,22 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
 
-    private string fixationTestScene = "FixationTestScene";
-    private string saccadeTestScene = "SaccadeTestScene";
-    private string blinkTestScene = "BlinkTestScene";
-    private string smoothPersuitScene = "SmoothPersuitTestScene";
-    private string mainMenuScene = "MainMenuScene";
+    [Header("Scene names")]
+    [SerializeField] private string fixationTestScene = "FixationTestScene";
+    [SerializeField] private string saccadeTestScene = "SaccadeTestScene";
+    [SerializeField] private string blinkTestScene = "BlinkTestScene";
+    [SerializeField] private string smoothPersuitScene = "SmoothPersuitTestScene";
+    [SerializeField] private string mainMenuScene = "MainMenuScene";
     
-    public void LoadFixationScene()
-    {
-        SceneManager.LoadScene(fixationTestScene);
-    }
+    public void LoadFixationScene() => SceneManager.LoadScene(fixationTestScene);
 
-    public void LoadSaccadeScene()
-    {
-        SceneManager.LoadScene(saccadeTestScene);
-    }
+    public void LoadSaccadeScene() => SceneManager.LoadScene(saccadeTestScene);
 
-    public void LoadBlinkScene()
-    {
-        SceneManager.LoadScene(blinkTestScene);
-    }
+    public void LoadBlinkScene() => SceneManager.LoadScene(blinkTestScene);
 
-    public void LoadSmoothPersuitScene()
-    {
-        SceneManager.LoadScene(smoothPersuitScene);
-    }
+    public void LoadSmoothPersuitScene() => SceneManager.LoadScene(smoothPersuitScene);
 
-    public void LoadMainMenu()
-    {
-        SceneManager.LoadScene(mainMenuScene);
-
-    }
+    public void LoadMainMenu() => SceneManager.LoadScene(mainMenuScene);
 
     public void ExitApplication()
     {
